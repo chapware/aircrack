4cc0d5b6662b07a20a503874b77897c7f7876795
Bryan Chapman <chapware@gmail.com>
2013-05-04 22:25:38 -0500

    aircrack

---
+++ src/osdep/packed.h
@@ -0,0 +1,19 @@
+/*-
+ *  Copyright (c) 2011 Chapware Technologies <chapware@gmail.com>
+ *
+ * pack structures
+ *
+ */
+
+#ifndef __AIRCRACK_NG_OSDEP_PACKED_H__
+#define __AIRCRACK_NG_OSDEP_PACKED_H__
+
+#ifndef __packed
+#define __packed __attribute__ ((__packed__))
+#endif /* __packed */
+
+#ifndef __aligned
+#define __aligned(n)
+#endif
+
+#endif /* __AIRCRACK_NG_OSEDEP_PACKED_H__ */
